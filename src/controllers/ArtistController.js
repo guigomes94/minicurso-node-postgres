438f2e50ddd0b56f3ef86e3d8d1b48e2d87a389a
Danilo Marques <danilomarques20@hotmail.com>
2019-11-22 22:34:57 -0300

    Criacao do model de artistas para conversar como banco de dados. Ajuste no controller dos artistas

--- src/controllers/ArtistController.js
+++ src/controllers/ArtistController.js
@@ -1,15 +1,16 @@
 import pool from '../config/conn';
+import ArtistModel from '../models/ArtistModel';
 
 class ArtistController {
   async index(req, res) {
 
     try {
-      const result = await pool.query('SELECT * FROM artist');
-      res.status(200).send(result.rows);
-
-    } catch (err) {
-      res.status(400).send({ "erro": err.message });
+      const result = await ArtistModel.findAll();
+      return res.json(result);
+    } catch(e) {
+      return res.json({message: e.message});
     }
+
     
   }
 
@@ -17,24 +18,38 @@ class ArtistController {
     const { id } = req.params;
 
     try {
-      const result = await pool.query(`SELECT * FROM artist WHERE idartist = ${id}`);
-      res.status(200).send(result.rows);
-
-    } catch (err) {
-      res.status(400).send({ "erro": err.message });
+      const result = await ArtistModel.findOne(id);
+      return res.json(result);
+    } catch (e) {
+      return res.json({message: e.message});
     }
+
+    // try {
+    //   const result = await pool.query(`SELECT * FROM artist WHERE idartist = ${id}`);
+    //   res.status(200).send(result.rows);
+
+    // } catch (err) {
+    //   res.status(400).send({ "erro": err.message });
+    // }
   }
 
   async store(req, res) {
     const { name } = req.body;
 
     try {
-      await pool.query(`INSERT INTO artist VALUES(nextval('artist_idartist_seq'),'${name}')`);
-      res.status(201).send({ message: "OK!" });
-      
-    } catch (err) {
-      res.status(404).send({ "erro": err.message });
+      const result = await ArtistModel.create(name);
+      return res.json(result);
+    } catch(e) {
+      return res.json({message: e.message});
     }
+
+    // try {
+    //   await pool.query(`INSERT INTO artist VALUES(nextval('artist_idartist_seq'),'${name}')`);
+    //   res.status(201).send({ message: "OK!" });
+      
+    // } catch (err) {
+    //   res.status(404).send({ "erro": err.message });
+    // }
   }
 
 
@@ -43,23 +58,21 @@ class ArtistController {
     const { name } = req.body;
 
     try {
-      await pool.query(`UPDATE artist SET name = '${name}' WHERE idartist = ${id}`);
-      res.status(200).send({ message: "OK!" });
-
-    } catch (err) {
-      res.status(404).send({ "erro": err.message });
-    } 
+      const result = await ArtistModel.findOneAndUpdate(id, name);
+      return res.json(result);
+    }  catch(e) {
+      return res.json({message: e.message});
+    }
   }
 
   async destroy(req, res) {
     const { id } = req.params;
 
     try {
-      await pool.query(`DELETE FROM artist WHERE idartist = ${id}`)
-      res.status(200).send({ message: "OK!" })
-
-    } catch (err) {
-      res.status(404).send({"erro": err.message});
+      const result = await ArtistModel.findAndDelete(id);
+      return res.json(result);
+    } catch(e) {
+      return res.json({message: e.message});
     }
   }
 
